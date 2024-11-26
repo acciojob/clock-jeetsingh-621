ff7527f7d4e3346e6ea4111d48828c57f65c8fcd
JITENDER <138762808+jeetsingh-621@users.noreply.github.com>
2024-11-26 12:07:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let timer =document.getElementById("timer");
+let currentdate = new Date();
+
+setInterval(()=>{
+	currentdate = new Date();
+
+	timer.innerHTML = currentdate.toLocaleString();
+},1000)
